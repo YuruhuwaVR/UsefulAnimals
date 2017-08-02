a76f507e43d9b8bbdf87b6cc5eeb53817d60eb5c
池田光 <Hikaru@pchan.local>
2017-08-02 10:08:26 +0900

    Add Animal abstruct class

---
+++ Assets/Scripts/Animal.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Animal : MonoBehaviour
+{
+
+	[SerializeField] protected int Id;
+	[SerializeField] protected int Weight;
+	[SerializeField] protected int Movespeed;
+	[SerializeField] protected int AttackPower;
+
+	protected abstract void SpecialAttack();
+}
