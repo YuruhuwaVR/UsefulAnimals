c7e735e198e8ea6b387f423a85681563835a1608
池田光 <Hikaru@pchan.local>
2017-08-02 09:52:13 +0900

    Add PlayerManager.cs to manage status of playing stage

---
+++ Assets/Scripts/PlayManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+
+public class PlayManager : MonoBehaviour
+{
+
+	private Stage _stage;
+	private Animal _currentAnimal;
+
+	public enum Phase
+	{
+		Start,
+		Select,
+		Ready,
+		Flying
+	}
+
+	private int _currentCost;
+	private bool _specialAttack; //これAnimalクラス？？
+	
+	
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void GameClear()
+	{
+		//ゲームクリアしたときの処理
+	}ß
+
+	void GameRestart()
+	{
+		//リスタートするときの処理
+	}
+
+	void GameFail()
+	{
+		//ゲームに失敗した時の処理
+	}
+
+	void Check()
+	{
+		//Animalが当たってクリアかどうか判定
+	}
+	
+}
