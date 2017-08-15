b32ebf066dbc5598da5205a924dc29b6b3e4ab21
神宮亜良太 <arata@jinguuaryoutanoMacBook-Pro.local>
2017-08-15 23:33:39 +0900

    Add function to change sprite and collider by click

--- Assets/Scripts/BlockCell.cs
+++ Assets/Scripts/BlockCell.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEditor;
+
 
 public class BlockCell : MonoBehaviour {
 
@@ -21,29 +23,41 @@ public class BlockCell : MonoBehaviour {
 	void Start(){
 		blockCellButton.onClick.AddListener (() => {
 			state = (BlockState)Enum.ToObject(typeof(BlockState), BlockChoiceManager.ChooseBlock);
+			ChangeBlock(state);
 		});
 	}
 
-	void Update(){
+
+	void ChangeBlock(BlockState state){
 		switch (state) {
 		case BlockState.Clear:
-			//blockCellImage.sprite = null;
-			blockCellImage.color = new Color(1.0f, 1.0f, 1.0f, 0.2f);
+			blockCellImage.sprite = AssetDatabase.GetBuiltinExtraResource<Sprite>("UI/Skin/UISprite.psd");
+			blockCellImage.color = Color.clear;
+			Destroy (this.GetComponent<Collider2D> ());
 			break;
 
 		case BlockState.Square:
-			//blockCellImage.sprite = null;
+			blockCellImage.sprite = AssetDatabase.GetBuiltinExtraResource<Sprite> ("UI/Skin/UISprite.psd");
 			blockCellImage.color = Color.red;
+			Destroy (this.GetComponent<Collider2D> ());
+			BoxCollider2D bc = this.gameObject.AddComponent (typeof(BoxCollider2D)) as BoxCollider2D;
+			bc.size = new Vector2(50f, 50f);
 			break;
 
 		case BlockState.Circle:
-			//blockCellImage.sprite = null;
+			blockCellImage.sprite = AssetDatabase.GetBuiltinExtraResource<Sprite> ("UI/Skin/Knob.psd");
 			blockCellImage.color = Color.blue;
+			Destroy (this.GetComponent<Collider2D>());
+			CircleCollider2D cc = this.gameObject.AddComponent(typeof(CircleCollider2D)) as CircleCollider2D;
+			cc.radius = 25f;
 			break;
-		    
+
 		case BlockState.Director:
-			//blockCellImage.sprite = null;
+			blockCellImage.sprite = AssetDatabase.GetBuiltinExtraResource<Sprite>("UI/Skin/Background.psd");
 			blockCellImage.color = Color.green;
+			Destroy (this.GetComponent<Collider2D> ());
+			BoxCollider2D dc = this.gameObject.AddComponent (typeof(BoxCollider2D)) as BoxCollider2D;
+			dc.size = new Vector2(50f, 50f);
 			break;
 		}
 	}
