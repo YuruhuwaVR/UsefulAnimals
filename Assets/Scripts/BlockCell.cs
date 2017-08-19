7c0981a24af12e36612da0e5aa8e2ea837536fdd
神宮亜良太 <arata@jinguuaryoutanoMacBook-Pro.local>
2017-08-19 21:04:37 +0900

    Add a function to apply gravity and to reset position

--- Assets/Scripts/BlockCell.cs
+++ Assets/Scripts/BlockCell.cs
@@ -10,6 +10,11 @@ public class BlockCell : MonoBehaviour {
 
 	[SerializeField] Image blockCellImage;
 	[SerializeField] Button blockCellButton;
+	[SerializeField] Rigidbody2D rb;
+	private Vector3 position;
+	private Quaternion rotation;
+	private bool isBlock;
+	public bool IsBlock {get{return isBlock;}}
 
 	enum BlockState{
 		Clear = 0,
@@ -31,9 +36,10 @@ public class BlockCell : MonoBehaviour {
 	void ChangeBlock(BlockState state){
 		switch (state) {
 		case BlockState.Clear:
-			blockCellImage.sprite = AssetDatabase.GetBuiltinExtraResource<Sprite>("UI/Skin/UISprite.psd");
+			blockCellImage.sprite = AssetDatabase.GetBuiltinExtraResource<Sprite> ("UI/Skin/UISprite.psd");
 			blockCellImage.color = Color.clear;
 			Destroy (this.GetComponent<Collider2D> ());
+			isBlock = false;
 			break;
 
 		case BlockState.Square:
@@ -42,6 +48,7 @@ public class BlockCell : MonoBehaviour {
 			Destroy (this.GetComponent<Collider2D> ());
 			BoxCollider2D bc = this.gameObject.AddComponent (typeof(BoxCollider2D)) as BoxCollider2D;
 			bc.size = new Vector2(50f, 50f);
+			isBlock = true;
 			break;
 
 		case BlockState.Circle:
@@ -50,6 +57,7 @@ public class BlockCell : MonoBehaviour {
 			Destroy (this.GetComponent<Collider2D>());
 			CircleCollider2D cc = this.gameObject.AddComponent(typeof(CircleCollider2D)) as CircleCollider2D;
 			cc.radius = 25f;
+			isBlock = true;
 			break;
 
 		case BlockState.Director:
@@ -58,7 +66,21 @@ public class BlockCell : MonoBehaviour {
 			Destroy (this.GetComponent<Collider2D> ());
 			BoxCollider2D dc = this.gameObject.AddComponent (typeof(BoxCollider2D)) as BoxCollider2D;
 			dc.size = new Vector2(50f, 50f);
+			isBlock = true;
 			break;
 		}
+	}
+
+	public void ApplyGravity(){
+		position = this.gameObject.transform.position;
+		rotation = this.gameObject.transform.rotation;
+		rb.bodyType = RigidbodyType2D.Dynamic;
+		rb.gravityScale = 0.5f;
+	}
+
+	public void Reset(){
+		rb.bodyType = RigidbodyType2D.Static;
+		this.gameObject.transform.position = position;
+		this.gameObject.transform.rotation = rotation;	
 	}
 }
---
+++ Assets/Scripts/PreviewManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PreviewManager : MonoBehaviour {
+	[SerializeField] GameObject blockArrangePanel;
+	[SerializeField] Button previewButton;
+	[SerializeField] Button resetButton;
+	private List<BlockCell> blockCells;
+	private List<BlockCell> dynamicCells;
+	private List<BlockCell> staticCells;
+
+	void Start(){
+		blockCells = new List<BlockCell> ();
+		dynamicCells = new List<BlockCell> ();
+		staticCells = new List<BlockCell> ();
+
+		previewButton.onClick.AddListener (() => {
+			Initialize();
+			GetBlockCells();
+			Preview();
+
+		});
+
+		resetButton.onClick.AddListener (() => {
+			Reset();
+		});
+	}
+
+	void Initialize(){
+		blockCells.Clear ();
+		dynamicCells.Clear ();
+		staticCells.Clear ();
+	}
+
+	void GetBlockCells(){
+		blockCells.AddRange(blockArrangePanel.GetComponentsInChildren<BlockCell> ());
+		foreach (BlockCell cell in blockCells) {
+			if (cell.IsBlock) {
+				dynamicCells.Add (cell);
+			}
+			else {
+				staticCells.Add (cell);
+			}
+		}
+	}
+	void Preview(){
+		foreach(BlockCell cell in staticCells){
+			cell.gameObject.SetActive (false);
+		}
+			
+		foreach (BlockCell cell in dynamicCells) {
+			cell.ApplyGravity ();
+		}
+	}
+
+	void Reset(){
+		foreach (BlockCell cell in dynamicCells) {
+			cell.Reset ();
+		}
+
+		foreach (BlockCell cell in staticCells) {
+			cell.gameObject.SetActive (true);
+		}
+	}
+}
