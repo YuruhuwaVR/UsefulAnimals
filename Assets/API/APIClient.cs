43df99d3506a39a0f856dbc1fea0879acc686f51
Kosuke Machida <matchbaske.3.27@gmail.com>
2017-09-03 16:29:02 +0900

    add apiclient

---
+++ Assets/API/APIClient.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class APIClient: MonoBehaviour {
+	string rootUrl = "http://localhost:1323/";
+	void ResponseFrom(HTTPRequest request)
+	{
+		string url = rootUrl + request.RelativeUrl;
+		WWW www = new WWW(url);
+		StartCoroutine("ConnectionEnd", www);
+	}
+
+	private IEnumerator WaitingForRequest(WWW www)
+	{
+		yield return www;
+		ConnectionEnd(www);
+	}
+
+	private void ConnectionEnd(WWW www)
+	{
+		// hogehoge
+	}
+}
